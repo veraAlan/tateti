117b811bc3e7a918be7be32e81c684bcf0dd13bd
veraAlan <93807967+veraAlan@users.noreply.github.com>
2021-11-15 19:52:01 -0300

    Agregados tateti.php y programaVeraAcostaYaitul.php
    Archivos php necesarios para hacer el juego de tateti.

---
+++ programaVeraAcostaYaitul.php
@@ -0,0 +1,71 @@
+<?php
+include_once("tateti.php");
+
+/**************************************/
+/***** DATOS DE LOS INTEGRANTES *******/
+/**************************************/
+
+/* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
+/* ... COMPLETAR ... */
+
+/**
+ * Vera Alan Cristian Gaston
+ *      Legajo FAI - 2622
+ *      Mail: cryssal220799@gmail.com
+ *      Usuario GitHub: veraAlan
+ */
+
+
+
+/**************************************/
+/***** DEFINICION DE FUNCIONES ********/
+/**************************************/
+
+
+
+
+
+
+
+
+/**************************************/
+/*********** PROGRAMA PRINCIPAL *******/
+/**************************************/
+
+//Declaración de variables:
+
+
+//Inicialización de variables:
+
+
+//Proceso:
+
+$juego = jugar();
+//print_r($juego);
+//imprimirResultado($juego);
+
+
+
+/*
+do {
+    $opcion = ...;
+
+    
+    switch ($opcion) {
+        case 1: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
+
+            break;
+        case 2: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
+
+            break;
+        case 3: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+
+            break;
+        
+            //...
+    }
+} while ($opcion != X);
+*/
